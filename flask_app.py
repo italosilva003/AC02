d72e18c40d49a84e5d7471b8727c13116588c669
Henrique Marinho <henriquemarinho11@hotmail.com>
2020-05-11 16:11:40 -0300

    AC04

---
+++ flask_app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/inicio')
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/inicio2')
+def outro_inicio():
+    return render_template('alternative_index.html')
+
+## Para rodae o projeto em desenvolvimento
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
